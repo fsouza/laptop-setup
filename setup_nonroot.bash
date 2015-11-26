bba9d7eb2c24db915c7b174d4b82ee100db96f50
Francisco Souza <f@souza.cc>
2015-11-25 22:16:06 -0200

    setup_nonroot: fix vim setup and also remove links before recreating

--- setup_nonroot.bash
+++ setup_nonroot.bash
@@ -17,17 +17,24 @@ popd
 
 pushd ${HOME}
 
+rm -f .gitconfig .gitignore_global .hgignore_global .hgrc .bashrc
+
 ln -s Projects/dotfiles/.gitconfig
 ln -s Projects/dotfiles/.gitignore_global
 ln -s Projects/dotfiles/.hgignore_global
 ln -s Projects/dotfiles/.hgrc
-
-rm .bashrc
 ln -s Projects/dotfiles/.bash_profile .bashrc
+
+rm -rf .vim
 
 git clone git@github.com:fsouza/vimfiles.git .vim
 cat >.vimrc <<EOF
 source $HOME/.vim/.vimrc
 EOF
+
+pushd .vim
+git submodule update --init --recursive
+mkdir swp
+popd
 
 popd
